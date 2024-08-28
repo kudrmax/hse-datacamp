8f26813d40a7ecbb8a4cb592bb7e7b843cd58b4c
kudrmax <mdkudryashov@edu.hse.ru>
2024-08-28 18:25:34 +0300

    Add dates

--- weather_parser.py
+++ weather_parser.py
@@ -1,6 +1,8 @@
 import time
+import datetime
+
 import pandas as pd
-from typing import List
+from typing import List, Tuple
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -17,7 +19,14 @@ class Parser:
         self.service = Service(executable_path=driver_path)
         self.options = webdriver.ChromeOptions()
 
-    def _parse_html(self, zip_code: int):
+    @staticmethod
+    def date_range_generator(start_date, end_date):
+        current_date = start_date
+        while current_date <= end_date:
+            yield current_date
+            current_date += datetime.timedelta(days=1)
+
+    def _parse_html(self, zip_code: int, date: datetime.date):
         self.driver = Chrome(service=self.service, options=self.options)
         self.driver.get(url)
 
@@ -39,17 +48,17 @@ class Parser:
         table = soup.find('table', {'class': 'mat-table'})
         return table
 
-    def _save_html(self, html, zip_code: int):
-        with open(f'html_tables/table_{zip_code}.html', 'w') as file:
+    def _save_html(self, html, zip_code: int, date: datetime.date):
+        with open(f'html_tables/table_{zip_code}_{date}.html', 'w') as file:
             file.write(html)
 
-    def _get_html_from_file(self, zip_code: int):
-        with open(f'html_tables/table_{zip_code}.html', 'r') as file:
+    def _get_html_from_file(self, zip_code: int, date: datetime.date):
+        with open(f'html_tables/table_{zip_code}_{date}.html', 'r') as file:
             html = file.read()
             return html
 
-    def _get_weather_data_from_html(self, html: str, zip_code: int):
-        df = pd.DataFrame(columns=['zip_code', 'time', 'temperature', 'humidity', 'wind_speed'])
+    def _get_weather_data_from_html(self, html: str, zip_code: int, date: datetime.date):
+        df = pd.DataFrame(columns=['zip_code', 'date', 'time', 'temperature', 'humidity', 'wind_speed'])
         soup = BeautifulSoup(html, 'html.parser')
         rows = soup.find('table', {'class': 'mat-table'}).find_all('tr')
         for row in rows:
@@ -58,6 +67,7 @@ class Parser:
             if not len(row_list) == 10:
                 continue
             row_data['zip_code'] = zip_code
+            row_data['date'] = date
             row_data['time'] = row_list[0].text.strip()
             row_data['temperature'] = row_list[1].text.strip().split()[0]
             row_data['humidity'] = row_list[3].text.strip().split()[0]
@@ -66,24 +76,34 @@ class Parser:
             new_row = pd.DataFrame([row_data])
             df = pd.concat([df, new_row], ignore_index=True)
 
-        with open(f'weather_data_csv/weather_data_{zip_code}.csv', 'w') as file:
+        with open(f'weather_data_csv/weather_data_{zip_code}_{date}.csv', 'w') as file:
             df.to_csv(file, index=False)
 
-    def save_weather_data(self, zip_codes: List[int], parse=False):
+    def save_weather_data(
+            self,
+            zip_codes: List[int],
+            start_date: datetime.date,
+            end_date: datetime.date,
+            parse=False
+    ):
         try:
             if parse:
                 for zip_code in zip_codes:
-                    html = self._parse_html(zip_code)
-                    self._save_html(html, zip_code)
+                    for date in self.date_range_generator(start_date, end_date):
+                        html = self._parse_html(zip_code, date)
+                        self._save_html(html, zip_code, date)
             for zip_code in zip_codes:
-                html = self._get_html_from_file(zip_code)
-                self._get_weather_data_from_html(html, zip_code)
+                for date in self.date_range_generator(start_date, end_date):
+                    html = self._get_html_from_file(zip_code, date)
+                    self._get_weather_data_from_html(html, zip_code, date)
         except Exception as e:
             print(e)
 
 
 if __name__ == '__main__':
     zip_codes = [125480]
+    start_date = datetime.date(2024, 6, 1)
+    end_date = datetime.date(2024, 6, 1)
     url = 'https://www.wunderground.com/history'
     driver_path = './driver/chromedriver'
     vpn_extension_path = ''
@@ -92,4 +112,4 @@ if __name__ == '__main__':
         url=url,
         driver_path=driver_path
     )
-    parser.save_weather_data(zip_codes, parse=False)
+    parser.save_weather_data(zip_codes, start_date, end_date, parse=True)
