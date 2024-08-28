0f0acae04cf497d597d7e9bb1bec8bb250ceb3fa
kudrmax <mdkudryashov@edu.hse.ru>
2024-08-28 17:18:30 +0300

    Add redirect

--- weather_parser.py
+++ weather_parser.py
@@ -1,23 +1,31 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
 class Parser:
     def __init__(self, url: str, driver_path: str):
         self.url = url
-        self.driver_path = driver_path
 
-        service = Service(executable_path=self.driver_path)
+        service = Service(executable_path=driver_path)
         options = webdriver.ChromeOptions()
-        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver = Chrome(service=service, options=options)
+
+    def _accept_cookies(self):
+        # self.driver.find_element(By.XPATH, '//*[@id="cookie"]').click()
+        # self.driver.find_element(By.CLASS_NAME, 'message-button').click()
+        # self.driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Accept all"]').click()
+        # self.driver.find_element(By.XPATH, "//button[text()='Accept all']").click()
+        # self.driver.find_element(By.XPATH, "//button[text()='Accept all']").click()
+        pass
 
     def _get_html_by_zip_code(self, zip_code: str):
         self.driver.get(url)
-        time.sleep(1)
 
         search_box = self.driver.find_element(By.ID, "historySearch")
         time.sleep(1)
@@ -25,10 +33,11 @@ class Parser:
         search_box.send_keys(Keys.RETURN)
         time.sleep(1)
 
+        search_box.submit()
         view_button = self.driver.find_element(By.ID, "dateSubmit")
-        view_button.send_keys(Keys.RETURN)
-        time.sleep(1)
+        view_button.click()
 
+        time.sleep(3)
         return self.driver.page_source
 
     def _get_weather_data_from_html(self, html: str):
@@ -55,10 +64,11 @@ class Parser:
             return html
 
     def _get_parse_by_zip_code(self, zip_code: str):
-        # html = self._get_html_by_zip_code(zip_code)
-        # self._save_html_to_file(html, save_to='weather_data.html')
-        html = self._get_html_from_file('weather_data.html')
+        html = self._get_html_by_zip_code(zip_code)
+        self._save_html_to_file(html, save_to='weather_data.html')
+        # html = self._get_html_from_file('weather_data.html')
         result = self._get_weather_data_from_html(html)
+        time.sleep(3)
 
     def parse(self, zip_code):
         try:
@@ -71,6 +81,10 @@ if __name__ == '__main__':
     zip_code = 10001
     url = 'https://www.wunderground.com/history'
     driver_path = './driver/chromedriver'
+    vpn_extension_path = ''
 
-    parser = Parser(url=url, driver_path=driver_path)
+    parser = Parser(
+        url=url,
+        driver_path=driver_path
+    )
     parser.parse(zip_code)
